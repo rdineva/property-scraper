cf0295f09b47dd6d2e2c8704ffc83ba1f9a703f0
rdineva <radinaxdineva@gmail.com>
2024-03-31 23:56:43 +0300

    Extract scraper functionality in separate file and get specific city results by query params

--- main.py
+++ main.py
@@ -1,54 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
-from firebase_db import save_property, get_properties
-from property import Property
-from dotenv import load_dotenv
-import os
+from scraper import scrape, save_scraped_data
 
 
-load_dotenv()
+BASE_URL = "https://sales.bcpea.org/properties"
+PLOVDIV_QUERY_PARAMS = "court=16&city=3598"
+SOFIA_QUERY_PARAMS = "court=28&city=4389"
 
-url = "https://sales.bcpea.org/properties?perpage=2000"
+# Sofia
+scraped_properties = scrape(f"{BASE_URL}?{SOFIA_QUERY_PARAMS}")
+save_scraped_data(scraped_properties)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-
-new_properties = []
-items = soup.find_all(class_="item__group")
-
-for item in items:
-    date = item.find(class_="date").text.strip()
-    title = item.find(class_="title").text.strip()
-    area = item.find(class_="category").text.strip()
-    price = item.find(class_="price").text.strip()
-
-    labels = item.find_all(class_="label")
-    infos = item.find_all(class_="info")
-
-    link = item.find("a", href=True)["href"]
-
-    town = address = term = announcement = ""
-
-    for index, (label, info) in enumerate(zip(labels, infos)):
-        key = label.text.strip()
-        value = info.text.strip()
-
-        if "НАСЕЛЕНО МЯСТО" in key:
-            town = value
-        elif "Адрес" in key:
-            address = value
-        elif "СРОК" in key:
-            term = value
-        elif "ОБЯВЯВАНЕ НА" in key:
-            announcement = value
-
-
-    property = Property(date, title, area, price, town, address, term, announcement, link)
-    new_properties.append(property)
-
-ref = get_properties()
-
-for property in new_properties:
-    same_properties = ref.order_by_child("link").equal_to(property.link).get()
-    if not same_properties:
-        save_property(property)
+# Plovdiv
+scraped_properties = scrape(f"{BASE_URL}?{PLOVDIV_QUERY_PARAMS}")
+save_scraped_data(scraped_properties)
---
+++ scraper.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+from firebase_db import save_property, get_properties
+from property import Property
+from typing import List
+
+
+def scrape(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    new_properties = []
+    items = soup.find_all(class_="item__group")
+
+    for item in items:
+        new_property = get_property_data(item)
+        new_properties.append(new_property)
+
+    return new_properties
+
+
+def get_property_data(item: str):
+    date = item.find(class_="date").text.strip()
+    title = item.find(class_="title").text.strip()
+    area = item.find(class_="category").text.strip()
+    price = item.find(class_="price").text.strip()
+
+    labels = item.find_all(class_="label")
+    infos = item.find_all(class_="info")
+
+    link = item.find("a", href=True)["href"]
+
+    town = address = term = announcement = ""
+
+    for index, (label, info) in enumerate(zip(labels, infos)):
+        key = label.text.strip()
+        value = info.text.strip()
+
+        if "НАСЕЛЕНО МЯСТО" in key:
+            town = value
+        elif "Адрес" in key:
+            address = value
+        elif "СРОК" in key:
+            term = value
+        elif "ОБЯВЯВАНЕ НА" in key:
+            announcement = value
+
+    new_property = Property(date, title, area, price, town, address, term, announcement, link)
+    return new_property
+
+
+def save_scraped_data(new_properties: List[Property]):
+    ref = get_properties()
+
+    for property in new_properties:
+        same_properties = ref.order_by_child("link").equal_to(property.link).get()
+        if not same_properties:
+            save_property(property)
