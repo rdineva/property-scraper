32697fc911da2b9c7a85e8b2217fe7b51b3800f0
rdineva <radinadineva@icloud.com>
2025-01-22 16:30:49 +0200

    Refactor project folder structure

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,4 @@ BASE_PROPERTIES_URL = f"{BASE_URL}/properties"
 PER_PAGE_PARAM = "perpage=1000"
 PLOVDIV_QUERY_PARAMS = "court=16&city=3598"
 SOFIA_QUERY_PARAMS = "court=28&city=4389"
-
-
 VARNNA_QUERY_PARAMS = "court=3&city=648"
